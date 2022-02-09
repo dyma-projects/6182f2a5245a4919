7208b20767e339a70b72836df6583d980c72bb32
Elise Desperrois <e.desperrois@cavas.fr>
2022-02-09 22:47:14 +0100

    ex1 + ex2

---
+++ src/app/exercice3/exercice3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercice3',
+  template: '<p> p1 </p> <p> p2 </p>',
+  styleUrls: ['./exercice3.component.css']
+})
+export class Exercice3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
